f40da007963cfab8ecfd94b52c0342ec3c1ffad1
Bongz26 <bongzdondas@gmail.com>
2025-05-19 16:08:29 +0200

    Update to use relative paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/ProcushionRetail/", // Base path for GitHub Pages
+  base: './', // Use relative paths
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
